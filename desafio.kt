76c2a9d04f4745d3fb4a567916ebc5388a2f2421
Lucas da Silva Veras <144183575+LucasVeras0812@users.noreply.github.com>
2024-01-17 14:09:43 -0300

    desafiolucas.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,53 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
+// Enum para representar os níveis de dificuldade
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+// Classe para representar um usuário
+data class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+// Classe abstrata para representar conteúdos educacionais
+abstract class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+// Classe que herda de ConteudoEducacional para representar um curso
+class Curso(nome: String, duracao: Int = 60, val nivel: Nivel) : ConteudoEducacional(nome, duracao)
 
+// Classe para representar uma formação
+class Formacao(val nome: String) {
+
+    val conteudos = mutableListOf<ConteudoEducacional>()
     val inscritos = mutableListOf<Usuario>()
-    
+
+    // Função para matricular um usuário na formação
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("$usuario matriculado na formação $nome")
+    }
+
+    // Função para adicionar um conteúdo à formação
+    fun adicionarConteudo(conteudo: ConteudoEducacional) {
+        conteudos.add(conteudo)
+        println("Conteúdo '${conteudo.nome}' adicionado à formação $nome")
+    }
+
+    // Função para listar os conteúdos disponíveis na formação
+    fun listarConteudos() {
+        println("Conteúdos disponíveis na formação $nome:")
+        conteudos.forEach { println("- ${it.nome}") }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Criando alguns objetos para testar o código
+    val usuario1 = Usuario("Lucas")
+    val usuario2 = Usuario("Estefany")
+
+    val cursoKotlin = Curso("Kotlin Básico", 30, Nivel.BASICO)
+    val cursoJava = Curso("Java Avançado", 45, Nivel.INTERMEDIARIO)
+
+    val formacaoTI = Formacao("Formação em TI")
+    formacaoTI.adicionarConteudo(cursoKotlin)
+    formacaoTI.adicionarConteudo(cursoJava)
+
+    formacaoTI.listarConteudos()
+    formacaoTI.matricular(usuario1)
+    formacaoTI.matricular(usuario2)
 }
